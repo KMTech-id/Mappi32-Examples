472dd197cf3b94df65c5bf3ed26b187abb92cf03
unknown <yutymanshetya@gmail.com>
2020-12-25 21:10:51 +0700

    add Mappi32_Anemometer

---
+++ mappi32_anemometer/mappi32_anemometer.ino
@@ -0,0 +1,34 @@
+//More Information at: https://www.aeq-web.com/
+//Version 2.0 | 11-NOV-2020
+
+const int RecordTime = 3; //Define Measuring Time (Seconds)
+const int SensorPin = 23;  //Define Interrupt Pin (GPIO23 @Mappi32)
+
+int InterruptCounter;
+float WindSpeed;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() {
+  meassure();
+  Serial.print("Wind Speed: ");
+  Serial.print(WindSpeed);       //Speed in km/h
+  Serial.print(" km/h - ");
+  Serial.print(WindSpeed / 3.6); //Speed in m/s
+  Serial.println(" m/s");
+}
+
+void meassure() {
+  InterruptCounter = 0;
+  attachInterrupt(digitalPinToInterrupt(SensorPin), countup, RISING);
+  delay(1000 * RecordTime);
+  detachInterrupt(digitalPinToInterrupt(SensorPin));
+  WindSpeed = (float)InterruptCounter / (float)RecordTime * 2.4;
+}
+
+void countup() {
+  InterruptCounter++;
+}
