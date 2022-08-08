12ffd00c8e8997cbd08ddb507153786851be990b
Rino DC <rinodc13@gmail.com>
2022-08-08 14:50:42 +0700

    change FREQ, syncWOrd, separator

--- mappi32_node_node_aht10_ubidots/mappi32_node_node_aht10_sender/lora.ino
+++ mappi32_node_node_aht10_ubidots/mappi32_node_node_aht10_sender/lora.ino
@@ -10,33 +10,37 @@ void lora_init()
   // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
-  LoRa.setSyncWord(0xF3);
-  Serial.println("LoRa Initializing OK!");
+  LoRa.setSyncWord(0x01);
+  Serial.println("LoRa Initializing OK! FREQ = " + String(FREQ));
 }
 
 void lora_merge_data()
 {
   Serial.println("Merge Data...");
   json_data = "";
-  json_data += "{" ;
+  json_data += "{\"" ;
   json_data += temperature_label; 
-  json_data += ":";
+  json_data += "\":";
   json_data += tem_data;
-  json_data += ",";
+  json_data += ",\"";
   json_data += humidity_label; 
-  json_data += ":";
+  json_data += "\":";
   json_data += hum_data;
   json_data += "}";
 
   Serial.println("json_data = " + json_data);
   
-  String lora_data = "*" + unique_id + "," + device_label + "," + json_data + "#";
+  lora_data = "*" + unique_id + separator + device_label + separator + json_data + "#";
   Serial.println("lora_data = " + lora_data);
 }
 
 void lora_send_data()
 {
+  
   Serial.println("Send Data...");
+  
+  int lora_data_len = lora_data.length();
+  Serial.println("lora_data_len = " + String(lora_data_len));
   LoRa.beginPacket();
   LoRa.print(lora_data);
   LoRa.endPacket();
--- mappi32_node_node_aht10_ubidots/mappi32_node_node_aht10_sender/mappi32_node_node_aht10_sender.ino
+++ mappi32_node_node_aht10_ubidots/mappi32_node_node_aht10_sender/mappi32_node_node_aht10_sender.ino
@@ -10,7 +10,7 @@ float tem_data = 0, hum_data = 0;
 #include <SPI.h>
 #include <LoRa.h>
 
-#define FREQ 922E0
+#define FREQ 922000000
 String unique_id = "F4022918";
 
 //define the pins used by the transceiver module
@@ -20,17 +20,20 @@ String unique_id = "F4022918";
 
 String lora_data;
 String json_data;
-
+char separator = '&';
 
 //Ubidots Dashboard
 String device_label = "mappi32";
 String temperature_label = "temp";
 String humidity_label = "humi";
 
+// LED
+#define LED 5
 
 void setup() {
+  pinMode(LED, OUTPUT);
   //initialize Serial Monitor
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("### LoRa Node 1 ###");
 
   //setup LoRa transceiver module
@@ -44,6 +47,7 @@ void loop() {
   aht10_read();
   lora_merge_data();
   lora_send_data();
+  blink_led();
   Serial.println("\r\n");
-  delay(10000);
+  delay(20000);
 }
