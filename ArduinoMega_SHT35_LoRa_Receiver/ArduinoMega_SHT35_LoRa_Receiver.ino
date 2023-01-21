81c8a21c2ee9a48fac0fd47248821e7986532a89
Syafanida Khakiki <110647138+syafanidaa@users.noreply.github.com>
2023-01-21 08:46:13 +0700

    Add files via upload

---
+++ ArduinoMega_SHT35_LoRa_Receiver/ArduinoMega_SHT35_LoRa_Receiver.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include <LoRa.h>
+#define ss 53
+#define rst 10
+#define dio0 9
+
+String data1, data2 ;
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  //  SPI.begin(52,50, 51, 53);
+  //  SPI.begin(14, 12, 13, 15);
+  LoRa.setPins(ss, rst, dio0);
+
+  //replace the LoRa.begin(---E-) argument with your location's frequency
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(915E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+  void loop() {
+
+    // try to parse packet
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+      String LoRaData;
+      Serial.println("Received packet\n ");
+      // received a packet
+      while (LoRa.available())
+      {
+        LoRaData = LoRa.readString();
+        Serial.println(LoRaData);
+
+      }
+
+      if ( LoRaData.charAt(0) == '#') {
+        int i = 1;
+        for (i; i < LoRaData.length(); i++) {
+          if (LoRaData.charAt(i) == ',') {
+            break;
+          }
+          else {
+            data1 += LoRaData.charAt(i);
+          }
+        }
+
+        int j = i + 1 ;
+        for (j; j < LoRaData.length(); j++) {
+          if (LoRaData.charAt(j) == '!') {
+            break;
+          }
+          else {
+            data2 += LoRaData.charAt(j);
+          }
+        }
+
+      }
+
+      Serial.print("Temperature: ");
+      Serial.println(data1);
+      Serial.print("Humidity: ");
+      Serial.println(data2);
+
+      data1 = "";
+      data2 = "";
+
+      delay (1000);
+
+    }
+  }
