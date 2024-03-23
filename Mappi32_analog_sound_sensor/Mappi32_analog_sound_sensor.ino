ea30c888f20f98995f4b001deee5eacd184d4b02
Yugo Mulyo Adi <143587866+Yugobaik@users.noreply.github.com>
2024-03-23 09:22:56 +0700

    Add files via upload

---
+++ Mappi32_analog_sound_sensor/Mappi32_analog_sound_sensor.ino
@@ -0,0 +1,12 @@
+const int soundSensorPin = A0; // Pin analog untuk sensor suara
+int soundValue = 0; // Variabel untuk menyimpan nilai pembacaan sensor suara
+
+void setup() {
+  Serial.begin(9600); // Inisialisasi komunikasi serial dengan baud rate 9600
+}
+
+void loop() {
+  soundValue = analogRead(soundSensorPin); // Membaca nilai dari sensor suara
+  Serial.println(soundValue); // Mengirim nilai pembacaan sensor suara ke serial monitor
+  delay(1000); // Delay 1 detik sebelum membaca kembali
+}
