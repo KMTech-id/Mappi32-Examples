f80c0d20146fcdf8ff2b2f908ba5f4d214b17319
Yugo Mulyo Adi <143587866+Yugobaik@users.noreply.github.com>
2024-03-23 09:23:23 +0700

    Add files via upload

---
+++ Mappi32_Bluetooth_LED/Mappi32_Bluetooth_LED.ino
@@ -0,0 +1,25 @@
+// relay low level trigger
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;  // Membuat instance dari kelas BluetoothSerial
+
+int relay1 = 17;  // Menentukan pin untuk relay1
+
+void setup() {
+  SerialBT.begin("Something");  // Menginisialisasi Bluetooth dengan nama "Something"
+  Serial.begin(115200);  // Menginisialisasi komunikasi serial dengan baud rate 115200
+
+  pinMode(relay1, OUTPUT);  // Mengatur pin relay1 sebagai output
+  digitalWrite(relay1, LOW);  // Awalnya, set relay1 ke kondisi LOW (mati)
+}
+
+void loop() {
+  if (SerialBT.available() > 0) {  // Memeriksa apakah ada data yang masuk dari Bluetooth
+    char command = SerialBT.read();  // Membaca perintah yang masuk
+    if (command == '1') {
+      digitalWrite(relay1, LOW);    // menyalakan relay1
+    } else if (command == '2') {
+      digitalWrite(relay1, HIGH);  // mematikan relay1
+    }
+  }
+}
