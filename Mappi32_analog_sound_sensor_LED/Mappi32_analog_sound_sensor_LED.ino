50785542b56f5c9c186e72791b40bbfd19ad61f5
diaz <91771027+yareyaredz@users.noreply.github.com>
2024-03-29 09:34:14 +0700

    Add files via upload

---
+++ Mappi32_analog_sound_sensor_LED/Mappi32_analog_sound_sensor_LED.ino
@@ -0,0 +1,30 @@
+int sensor = 32;  // Ubah nilai pin sensor sesuai dengan koneksi hardware
+int led1 = 18;    // Ubah nilai pin LED sesuai dengan koneksi hardware
+
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  int v_sensor = analogRead(sensor);
+  Serial.println(v_sensor);
+
+  if (v_sensor > 650) {
+    delay(100);
+
+    while (true) {
+      digitalWrite(led1, HIGH);
+      v_sensor = analogRead(sensor);
+
+      if (v_sensor > 650) {
+        break;
+      }
+    }
+  } else {
+    digitalWrite(led1, LOW);
+  }
+
+  delay(100);
+}
