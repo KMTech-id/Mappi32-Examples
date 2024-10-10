d8bb44045d675b5ddaa9b1e8419a8c5fb77995ff
Yugo Mulyo Adi <143587866+Yugobaik@users.noreply.github.com>
2024-10-10 09:41:11 +0700

    Add files via upload

---
+++ Mappi32_Bluetooth_Receiver/Mappi32_Bluetooth_Receiver.ino
@@ -0,0 +1,24 @@
+// Getting Started with Bluetooth on Mappi32 //
+// @yugoyokaii . Video KMTek Indonesia //
+
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("********"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+
+}
+
+void loop() {
+  if (SerialBT.available()) {
+  int data_terima = SerialBT.read();
+  Serial.write(data_terima);
+  SerialBT.write(data_terima);
+ }
+}
